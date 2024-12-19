5379fa91dd8e1f1e5b5d4adb0c5e650f2916d26c
Alexy Heitz <aheitz@k1r2p1.42mulhouse.fr>
2024-12-19 12:39:24 +0100

    "Initial commit with all informations"

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CPP-00
 The first module of C++ designed to understand the specifities of the language when compared to C.
+
+GPT for the project: https://chatgpt.com/g/g-6763f04f6f948191a6b12e710af438dc-guide-cpp-00
---
+++ ex00/megaphone.cpp
@@ -0,0 +1,11 @@
+/*********************************************************************************
+*                              Author: Alexy Heitz                               *
+*               File Name: /home/aheitz/CPP-00/ex00/megaphone.cpp                *
+*                   Creation Date: December 19, 2024 12:35 PM                    *
+*                    Last Updated: December 19, 2024 12:38 PM                    *
+*                              Source Language: cpp                              *
+*                                                                                *
+*                            --- Code Description ---                            *
+*                   Just to make sure that everybody is awake!                   *
+*********************************************************************************/
+
