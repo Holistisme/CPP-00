39ffab35f12ae3189380540f555f5368bdb39e6b
holistisme <alexy.pa.heitz@gmail.com>
2024-12-19 15:33:57 +0100

    ex00 done!

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -9,3 +9,44 @@
 *                   Just to make sure that everybody is awake!                   *
 *********************************************************************************/
 
+/********************************************************************************/
+
+#include <cctype>
+#include <iostream>
+
+typedef int				index;
+typedef char			*string;
+
+#define	TO_UPPER(c)		static_cast<char>(std::toupper(static_cast<unsigned char>(c)));
+#define WRITE_SPACE		std::cout << " ";
+#define WRITE_NEWLINE	std::cout << std::endl;
+#define MAKE_NOISE		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+
+/********************************************************************************/
+
+/**
+ * @brief Referring the arguments as a sentence in capital letters.
+ * 
+ * @param argc The number of program parameters, in addition to the executable.
+ * @param argv The table of character strings, with the executable and the arguments.
+ * @return Will always return "EXIT_SUCCESS"
+ */
+int main(int argc, char *argv[]) {
+	index word = 1;
+
+	if (argc > 1) {
+		while (argv[word]) {
+			index letter = 0;
+
+			while (argv[word][letter])
+				std::cout << TO_UPPER(argv[word][letter++]);
+			if (argv[++word])
+				WRITE_SPACE;
+		}
+		WRITE_NEWLINE;
+	}
+	else
+		MAKE_NOISE;
+
+	return (0);
+}
