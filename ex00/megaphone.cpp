ba7a7ebd8325f04f8660eb5bf80607e4c9c183ae
holistisme <alexy.pa.heitz@gmail.com>
2024-12-19 15:38:50 +0100

    Fix updated dates

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,7 +2,7 @@
 *                              Author: Alexy Heitz                               *
 *               File Name: /home/aheitz/CPP-00/ex00/megaphone.cpp                *
 *                   Creation Date: December 19, 2024 12:35 PM                    *
-*                    Last Updated: December 19, 2024 12:38 PM                    *
+*                    Last Updated: December 19, 2024 03:38 PM                    *
 *                              Source Language: cpp                              *
 *                                                                                *
 *                            --- Code Description ---                            *
